c082d1548777b2e713a737fc1e664b648b393439
khan162 <umarahmedkhank6@gmail.com>
2023-01-04 14:05:02 -0500

    webpack configured completelt and new folders created

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 import { resolve } from "path";
 
-export const entry = "./src/index.js";
+export const entry = "./src/app.js";
 export const output = {
     filename: "main.js",
     path: resolve(__dirname, "dist"),
