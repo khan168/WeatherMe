1256222c74ccc4d3c201c6bdf4f141d525b7ae8a
khan162 <umarahmedkhank6@gmail.com>
2023-01-04 14:02:54 -0500

    webpack configured

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ dotenv.config();
 
 //steps to complete
 /*
-1.create factory instead of weather obj
+1.create module pattern instead of weather obj
 2.create a display module - follow solid principle
 3.async await instead of chained .then
 */
-let Weather = {
-  key: process.env.API_KEY,
-  fetchweather: function (city) {
+
+const weather_module =(()=>{
+  const key= process.env.API_KEY
+  const fetchweather=function (city) {
     fetch(
       "https://api.openweathermap.org/data/2.5/weather?q=" +
         city +
@@ -24,8 +25,8 @@ let Weather = {
       .catch((err) => {
         console.log(err);
       });
-  },
-  displayWeather: function (data) {
+  }
+  const displayWeather= function (data) {
     const { name } = data;
     const { description, icon } = data.weather[0];
     const { temp, humidity } = data.main;
@@ -37,21 +38,61 @@ let Weather = {
     document.querySelector(".description").innerText = description;
     document.querySelector(".icon").src =
       "http://openweathermap.org/img/wn/" + icon + ".png";
-  },
-  search: function () {
+  }
+  const search=function () {
     this.fetchweather(document.querySelector(".search-bar").value);
-  },
-};
+  }
+  return {
+    search,fetchweather
+  }
+});
+
+
+// let Weather = {
+//   key: process.env.API_KEY,
+//   fetchweather: function (city) {
+//     fetch(
+//       "https://api.openweathermap.org/data/2.5/weather?q=" +
+//         city +
+//         "&appid=" +
+//         key +
+//         "&units=metric"
+//     )
+//       .then((response) => response.json())
+//       .then((data) => {
+//         this.displayWeather(data);
+//       })
+//       .catch((err) => {
+//         console.log(err);
+//       });
+//   },
+//   displayWeather: function (data) {
+//     const { name } = data;
+//     const { description, icon } = data.weather[0];
+//     const { temp, humidity } = data.main;
+//     const { speed } = data.wind;
+//     document.querySelector(".city").innerText = "Weather in " + name;
+//     document.querySelector(".temp").innerText = temp + " ° C";
+//     document.querySelector(".wind").innerText =
+//       "Wind Speed " + speed + " Km/hr";
+//     document.querySelector(".description").innerText = description;
+//     document.querySelector(".icon").src =
+//       "http://openweathermap.org/img/wn/" + icon + ".png";
+//   },
+//   search: function () {
+//     this.fetchweather(document.querySelector(".search-bar").value);
+//   },
+// };
 
 document.querySelector(".search button").addEventListener("click", function () {
-  Weather.search();
+  weather_module.search();
 });
 document
   .querySelector(".search-bar")
   .addEventListener("keyup", function (event) {
     if (event.key == "Enter") {
-      Weather.search();
+      weather_module.search();
     }
   });
 
-Weather.fetchweather("denver");
+weather_module.fetchweather("denver");
